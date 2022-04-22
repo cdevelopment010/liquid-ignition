d6728a87c830ad0e2cb626b3752a92866bd75134
cdevelopment010 <cdevelopment010@gmail.com>
2022-04-22 15:55:49 +0100

    Feat: merge redesign to main

--- index.js
+++ index.js
@@ -1,43 +1,46 @@
-const pageHeight = window.innerHeight; 
-// const video = document.getElementById('video');
-// const showVideoEl = document.querySelectorAll('.show-video')[0]; 
-const sections = document.querySelectorAll('.scroll-section');
-// const addVideo = document.querySelector('.scroll-up-add-video');
+const hero = document.querySelector('header'); 
+const left = document.querySelector('.left'); 
+const right = document.querySelector('.right'); 
+const logo = document.querySelector('.logo'); 
+const navLi = document.querySelectorAll('nav li'); 
+
+logo.addEventListener('click', () => {
+    left.style.setProperty('--left', 50); 
+    right.style.setProperty('--right', -50); 
+})
+
+navLi.forEach(li => {
+    li.addEventListener('click', () => {
+        left.style.setProperty('--left', -200); 
+        right.style.setProperty('--right', -100); 
+    })
+})
+
+hero.addEventListener('wheel', function( e ) {
+    let heroHeight = hero.offsetHeight; 
+    let percentScroll =  (window.pageYOffset * 200)/ heroHeight;
+    // console.log((window.pageYOffset / heroHeight*150) + 25);
+    // console.log("top", (window.pageYOffset / heroHeight) + 25); 
 
 
 
-// sections.forEach(section => {
-//     section.addEventListener('wheel', function(e) {
-//         let scrollTo;
-//         //scrollup 
-//         if (e.deltaY < 0) {
-//             scrollTo = window.scrollY - pageHeight > 0 ? window.scrollY - pageHeight : 0;
-//             getPosition(scrollTo); 
-//             // hideShowVideo(); 
-//         }
-//         // scroll down
-//         if (e.deltaY > 0) {
 
-//             scrollTo = window.scrollY + pageHeight < document.body.clientHeight ? window.scrollY + pageHeight : document.body.clientHeight;
-//             getPosition(scrollTo); 
-//         }
-//         return; 
-//     })
-// })
+    let leftPos = getComputedStyle(left).getPropertyValue('--left'); 
+    let rightPos = getComputedStyle(right).getPropertyValue('--right'); 
+    leftPos = 50 +  (1 - (percentScroll * 2)); 
+    rightPos = -50 +  (1 + (percentScroll)); 
 
-// showVideoEl.addEventListener('wheel', function(e){
-//     if (e.deltaY > 0) {
-//         setTimeout(() => {
-//             video.classList.remove('fixed-position');
-//         }, 300)
-//     }
-// })
-// addVideo.addEventListener('wheel', function(e){
-//     if (e.deltaY < 0) {
-//         video.classList.add('fixed-position');
-//     }
-// })
+    console.log("Left:", leftPos);
+    console.log("right:", rightPos);
+     
 
-// function getPosition(pos) {
-//     window.scrollTo({ top: pos, behavior: 'smooth'});
-// }
+    left.style.setProperty('--left', leftPos); 
+    right.style.setProperty('--right', rightPos); 
+    // left.style.setProperty('--top', (window.pageYOffset / heroHeight) + 25); 
+})
+
+window.addEventListener('change', () => {
+    if (window.pageYOffset > hero.offsetHeight) {
+        console.log("hide left/right"); 
+    }
+})
